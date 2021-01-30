5bf0dcb6d9578f9d49597954123c26be8e743274
Paullo B. Felipe <paullotsw@gmail.com>
2021-01-30 00:50:46 -0300

    Timer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    float currentTime;
+    [SerializeField] int startingTime = 20;
+    [SerializeField] Text timeText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTime();
+    }
+
+
+    void UpdateTime()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        
+       
+
+        timeText.text = "Time: " + Mathf.RoundToInt(currentTime).ToString();
+        
+
+        if (currentTime <= 0)
+            currentTime = 0;
+
+    }
+
+
+
+
+
+
+
+}
